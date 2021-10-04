49f0bbf4658bc3044e25a2c03a63ac441db63513
Johnson <yichenglau@gmail.com>
2021-10-04 10:14:18 -0700

    Create home page layout, navigation works.

--- app/src/main/java/com/example/airsick/HomeActivity.java
+++ app/src/main/java/com/example/airsick/HomeActivity.java
@@ -2,13 +2,27 @@ package com.example.airsick;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_home);
+
+        getSupportActionBar().hide();
+
+        Button buttonMap = (Button) findViewById(R.id.buttonMap);
+
+        buttonMap.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                setContentView(R.layout.activity_map);
+            }
+        });
+
     }
 }
